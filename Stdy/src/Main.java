6f545af7aac69bb7ddf6a77b402438128d7bb2ce
mouse <xiaotianbai0206@gmail.com>
2017-09-15 16:06:49 -0400

    091501

---
+++ Stdy/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //     String s1 = scan.nextLine();
+        double d1 = -100000 / 3.0;
+        System.out.printf("%,#(.2f", d1);
+        System.out.println("Hello World!");
+    }
+}
