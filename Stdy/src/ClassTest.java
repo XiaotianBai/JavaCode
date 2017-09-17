c73a70907343f2c9b4c9188f2fc1e282d17a8024
mouse <xiaotianbai0206@gmail.com>
2017-09-17 12:28:46 -0400

    091501

---
+++ Stdy/src/ClassTest.java
@@ -0,0 +1,42 @@
+public class ClassTest {
+    Human BXT = new Human();
+    private String name;
+    private String job;
+
+    public ClassTest() {
+    }
+
+    public ClassTest(String s1, String s2) {
+        this.name = s1;
+        this.job = s2;
+
+    }
+
+    public String GetName() {
+        return this.name;
+    }
+
+    public String GetJob() {
+        return this.job;
+    }
+
+    public int GetBXTAge() {
+        return BXT.jimbo();
+    }
+}
+
+class SubTest extends ClassTest {
+
+    public SubTest() {
+    }
+
+    public SubTest(String s1, String s2) {
+        super(s1, s2);
+
+    }
+
+    public String GetName() {
+        String name = super.GetName();
+        return "gg";
+    }
+}
--- Stdy/src/Main.java
+++ Stdy/src/Main.java
@@ -1,12 +1,52 @@
 import java.util.Scanner;
 
+/**
+ * my first java program
+ * helps to understand java
+ */
 public class Main {
 
     public static void main(String[] args) {
+        ClassTest ct1 = new ClassTest("ss", "dd");
         Scanner scan = new Scanner(System.in);
+        String s1 = "abcdefg";
+        String s2 = s1.replace("a", "z");
+        System.out.println(s1);
+        System.out.println(s2);
         //     String s1 = scan.nextLine();
         double d1 = -100000 / 3.0;
-        System.out.printf("%,#(.2f", d1);
+        System.out.printf("%s %s", d1, ct1.GetName());
         System.out.println("Hello World!");
+        Human BXT = new Human();
+        System.out.println(BXT.nestjimbo());
+        System.out.println(Human.poo());
     }
 }
+
+class Human {
+    private static String s = "pooo";
+    private int age;
+    private int height;
+
+    public static String poo() {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(999);
+    }
+
+    public int jimbo() {
+        age = 10;
+        height = 20;
+        return age;
+    }
+
+    public int nestjimbo() {
+        ClassTest ct = new ClassTest("oo", "kk");
+        return ct.GetBXTAge();
+    }
+
+
+}
+
