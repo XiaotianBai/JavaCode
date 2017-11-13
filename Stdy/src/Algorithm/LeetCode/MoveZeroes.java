5d590bebfc24369cd0b2682b147ae2235f926df4
mouse <xiaotianbai0206@gmail.com>
2017-11-12 22:31:17 -0500

    11.12

---
+++ Stdy/src/Algorithm/LeetCode/MoveZeroes.java
@@ -0,0 +1,23 @@
+package Algorithm.LeetCode;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void main(String[] args)
+    {
+        int[] nums = {1,0};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums)
+    {
+        if(nums.length == 1) return;
+        int zeroes = 0;
+        for(int i = 0; i < nums.length; i ++)
+        {
+            if(nums[i] == 0) zeroes++;
+            else if(zeroes > 0){nums[i - zeroes] = nums[i]; nums[i] = 0;}
+        }
+    }
+}
