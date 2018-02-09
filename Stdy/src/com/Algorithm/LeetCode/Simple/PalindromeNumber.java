b8d48fe7b93ac5ca2b888732f86a67749a516be8
mouse <xiaotianbai0206@gmail.com>
2018-02-08 22:01:45 -0500

    2.4 update

---
+++ Stdy/src/com/Algorithm/LeetCode/Simple/PalindromeNumber.java
@@ -0,0 +1,13 @@
+package com.Algorithm.LeetCode.Simple;
+
+public class PalindromeNumber {
+    public static void main(String[] args){
+
+    }
+
+    private static boolean isPalindrome(int x) {
+        //we do it by comparing digits from both end
+        int div  = 1;
+        whi
+    }
+}
