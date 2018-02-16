2230ebb59f035154d712e3ed4c4e57fe06848084
mouse <xiaotianbai0206@gmail.com>
2018-02-16 05:53:52 -0500

    2.4 update

---
+++ Stdy/src/com/Algorithm/LeetCode/Simple/IsomorphicStrings.java
@@ -0,0 +1,15 @@
+package com.Algorithm.LeetCode.Simple;
+
+public class IsomorphicStrings {
+    public static void main(String[] args){
+
+    }
+
+    private static boolean isIsomorphic(String s, String t) {
+//        if(s.length() != t.length()) return false;
+        if(s.length() == 0) return true;
+
+    }
+
+
+}
